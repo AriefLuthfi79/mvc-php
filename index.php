af7820654ecf2b32e4e6643fa82e4bca70b309a8
ariefluthfi79 <ariefluthfi799@gmail.com>
2019-01-20 21:42:38 +0700

    committed

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
 <?php
 
 require_once __DIR__ . '/vendor/autoload.php';
-
-use Bookstore\Models\SaleModel;
-use Bookstore\Domain\Sale;
-
-$db = new PDO(
-	"mysql:host=mariadb;dbname=bookstore",
-	"root",
-	"qwerty123"
-);
